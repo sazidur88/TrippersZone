9d502556cc094b5201bc3a482e631119010f3433
sazidurrahman <sazidur.rahman@northsouth.edu>
2019-11-05 03:38:42 +0600

    framework added

---
+++ Main Project/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sazidur/Desktop/fall2019.cse327.1.t4/Main Project/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
